7ff744657bce7c1b0e5552bf3bcc99750e0ddf1d
Erica Wolf <ebwolf@gmail.com>
2024-12-17 16:28:52 +0100

    Updated README

--- buzzer.sh
+++ buzzer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Set volume to 80%
+amixer -D pulse sset Master 80%
+
 # Buzzer script for alarm.sh
 
+# Launch Firefox with Deutshlandfunk livestream
 firefox https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3?aggregator=web
 
