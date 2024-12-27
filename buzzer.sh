b5ed7f51a4157439597086085b95df3489a5bdb2
Erica Wolf <ebwolf@gmail.com>
2024-12-27 11:43:07 +0100

    Switching playback from Firefox to FFplay (from ffmpeg)

--- buzzer.sh
+++ buzzer.sh
@@ -6,10 +6,10 @@ bluetoothctl disconnect
 # Unmute the volume
 amixer sset 'Master' unmute
 # Set volume to 80%
-amixer sset 'Master' 90%
+amixer sset 'Master' 100%
 
 # Buzzer script for alarm.sh
 
 # Launch Firefox with Deutshlandfunk livestream
-firefox https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3?aggregator=web
-
+#firefox https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3?aggregator=web
+ffplay -fs https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3?aggregator=web
